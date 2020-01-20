bcf65ef94f2d2f59d5d60f11a3cf7a62538684c7
artekhrsn <artek.horosin@gmail.com>
2020-01-20 18:33:11 +0100

    add object

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-var allPersons = [];
+var allPersons = [{
+    id: 1,
+    name: "Artek",
+    lastname: "Horosin",
+    gender: "male",
+    phone: 845968458,
+    email: "artek.horosin@gmail.com",
+    date: new Date(),
+
+}];
 
 window.onload = () => {
 
