a628c0a67861be85f4ba625bc1560bb9a8d715b1
MPuzio <martynapuzio93@gmail.com>
2020-01-21 00:52:45 +0100

    button type added, function checkEmail and validateRequiredInputs added

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-var allPersons = [{
-    id: 1,
-    name: "Artek",
-    lastname: "Horosin",
-    gender: "male",
-    phone: 845968458,
-    email: "artek.horosin@gmail.com",
-    date: new Date(),
-
-}];
+// var allPersons = [
+//   {
+//     id: 1,
+//     name: "Artek",
+//     lastname: "Horosin",
+//     gender: "male",
+//     phone: 845968458,
+//     email: "artek.horosin@gmail.com",
+//     date: new Date()
+//   }
+// ];
 
 window.onload = () => {
-const buttonAdd = document.getElementById("addBtn");
-const allPersons = [];
-buttonAdd.addEventListener("click",event => {
-    event.preventDefault();
-let 
-});
-}
+  const buttonAdd = document.getElementById("addBtn");
+  const allPersons = [];
 
+  //   buttonAdd.addEventListener("click", event => {
+  //     event.preventDefault();
+  //     let;
+  //   });
+  function checkEmail() {
+    let email = document.getElementById("email");
+    email = email.value;
+    console.log(email);
+    const mailRegex = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i;
+    if (mailRegex.test(email)) {
+      console.log("E-mail is ok");
+    } else {
+      console.log("Not correct e-mail format");
+    }
+  }
+  function validateRequiredInputs() {
+    var requiredInputs = document.querySelectorAll("[required]");
+    console.log(requiredInputs);
+
+    requiredInputs.forEach(element => {
+      var input = element.value;
+      var inputRegex = /^.+/s;
+      if (input === "" || inputRegex.test(input)) {
+        alert(
+          "At least the required fields have to be filled in to add the person"
+        );
+        return;
+      }
+    });
+  }
+  checkEmail();
+  validateRequiredInputs();
+};
