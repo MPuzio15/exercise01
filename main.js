29c3b4cb7a4f7f7816ec7a4d1e3a4b2d65a8f09b
MPuzio <martynapuzio93@gmail.com>
2020-01-21 17:13:58 +0100

    function checkEmail and validateRequiredfields  added

--- main.js
+++ main.js
@@ -69,6 +69,36 @@ window.onload = () => {
     }
   });
 
+  function checkEmail() {
+    let email = document.getElementById("email");
+    email = email.value;
+    console.log(email);
+    const mailRegex = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i;
+    if (mailRegex.test(email)) {
+      console.log("E-mail is ok");
+    } else {
+      console.log("Not correct e-mail format");
+    }
+  }
+  function validateRequiredInputs() {
+    var requiredInputs = document.querySelectorAll("[required]");
+    console.log(requiredInputs);
+
+    requiredInputs.forEach(element => {
+      var input = element.value;
+      var inputRegex = /^.+/s;
+      if (input === "" || inputRegex.test(input)) {
+        alert(
+          "At least the required fields have to be filled in to add the person"
+        );
+        return;
+      }
+    });
+  }
+  checkEmail();
+  validateRequiredInputs();
+
+
   //   function goToTheNextInput() {
   //     var inputs = document.querySelectorAll("input");
   //     inputs.forEach(
